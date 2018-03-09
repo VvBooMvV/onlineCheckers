f21fc6a7f3858239a5cbaae3f6e697d7f1da692d
Sam Greenberg <11greenberg.sam@gmail.com>
2018-03-08 22:59:34 -0500

    Got game working.

--- Downloads/cs451checkers/src/main/resources/static/js/new-main.js
+++ Downloads/cs451checkers/src/main/resources/static/js/new-main.js
@@ -21,17 +21,17 @@ window.onload = function() {
     var pieces = [];
     var tiles = [];
     var ActionEnum = Object.freeze({"move":456, "jump":123});
-    var PlayerTurnEnum = Object.freeze({"red":100, "black":101});
+    var PlayerTurnEnum = Object.freeze({"red":1, "black":2});
 
     //distance formula
     var distance = function (x1, y1, x2, y2) {
         return Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
     };
 
-    function Piece(linkedElement, color) {
+    function Piece(linkedElement, position, color) {
         //TODO: going to have to initialize pieces and tiles in nested for loop.  That way, it can have both as a
         //comparable ij value.
-        this.position = undefined;
+        this.position = position;
         this.linkedElement = linkedElement;
         this.kinged = false;
         //TODO: enforce 0 for black, 1 for red?
@@ -42,25 +42,32 @@ window.onload = function() {
             //TODO: call update board state
         };
         this.move = function (tile) {
+            this.linkedElement.removeClass('selected');
             //TODO: check if it's a valid move.
             //Hand a tile in, use the current tile position this piece has to check for validity
             //If it's not a valid move, just eat it.
-            if (Board.isValidMove(tile.position[0], tile.position[y])) {
-                if (owner === 0 && !this.kinged) {
-                    //Checks for unkinged piece moving backwards for black
+            if (Board.isValidMove(tile.position[0], tile.position[1])) {
+                console.log("In valid move check");
+                if (this.owner === PlayerTurnEnum.red && this.kinged === false) {
+                    //Checks for unkinged piece moving backwards for red
                     if (tile.position < this.position) {
                         return false;
                     }
-                } else if (owner === 1 && !this.kinged) {
-                    //Checks for unkinged piece moving backwards for red
+                } else if (this.owner === PlayerTurnEnum.black && this.kinged === false) {
+                    //Checks for unkinged piece moving backwards for black
                     if (tile.position > this.position) {
                         return false;
                     }
                 }
 
-                //Set new position to tile position
-                this.position = tile.position;
+                Board.board[this.position[0]][this.position[1]] = 0;
+                Board.board[tile.position[0]][tile.position[1]] = this.owner;
 
+                //Set new position to tile position
+                this.position = [tile.position[0], tile.position[1]];
+
+                this.linkedElement.css('top', Board.dictionary[this.position[0]]);
+                this.linkedElement.css('left', Board.dictionary[this.position[1]]);
                 //TODO: add board functionality to keep track of player tiles
                 //TODO: add board functionality to update which player is on which tile
 
@@ -69,27 +76,40 @@ window.onload = function() {
                 //TODO: use websocket to send spring whatever new information via board object
                 return true;
             } else {
+                console.log("Not a valid move");
                 //TODO: eat it
                 return false;
             }
         };
+
+        //tests if piece can jump anywhere
+        this.canJumpAgain = function () {
+            return (this.canJump([this.position[0] + 2, this.position[1] + 2]) ||
+                this.canJump([this.position[0] + 2, this.position[1] - 2]) ||
+                this.canJump([this.position[0] - 2, this.position[1] + 2]) ||
+                this.canJump([this.position[0] - 2, this.position[1] - 2]));
+        };
+
         this.canJump = function (newTile) {
-            var dx = newTile.position[1] - this.position[1];
-            var dy = newTile.position[0] - this.position[0];
-            if (owner === 0 && !this.kinged) {
+            var dx = newTile[1] - this.position[1];
+            var dy = newTile[0] - this.position[0];
+            if (this.owner === PlayerTurnEnum.red && this.kinged === false) {
                 //Checks for unkinged piece moving backwards for black
-                if (newTile.position < this.position) {
+                if (newTile[0] < this.position[0]) {
+                    console.log("Failed to jump");
                     return false;
                 }
-            } else if (owner === 1 && !this.kinged) {
+            } else if (this.owner === PlayerTurnEnum.black && this.kinged === false) {
                 //Checks for unkinged piece moving backwards for red
-                if (newTile.position > this.position) {
+                if (newTile[0] > this.position[0]) {
+                    console.log("Failed to jump");
                     return false;
                 }
             }
 
-            if (newTile.position[0] > 7 || newTile.position[1] > 7
-                || newTile.position[0] < 0 || newTile.position[1] < 0) {
+            if (newTile[0] > 7 || newTile[1] > 7
+                || newTile[0] < 0 || newTile[1] < 0) {
+                console.log("Failed to jump 2");
                 //out of bounds check
                 return false;
             }
@@ -97,28 +117,32 @@ window.onload = function() {
             //Get piece in between
             var tileToCheckx = this.position[1] + dx / 2;
             var tileToChecky = this.position[0] + dy / 2;
-            if (!Board.isValidMove(tileToCheckx, tileToChecky) && Board.isValidMove(newTile.position[0], newTile.position[1])) {
-                return true;
+            if (!Board.isValidMove(tileToChecky, tileToCheckx) && Board.isValidMove(newTile[0], newTile[1])) {
+                //return true;
+                for (pieceIndex in pieces) {
+                    if (pieces[pieceIndex].position[0] === tileToChecky && pieces[pieceIndex].position[1] === tileToCheckx) {
+                        if (this.owner !== pieces[pieceIndex].owner) {
+                            console.log("not sure");
+                            return pieces[pieceIndex];
+                        }
+                    }
+                }
             } else {
+                console.log("Failed to jump 3");
                 return false;
             }
         };
         this.jump = function (tile) {
             //TODO: calculate piece to be removed
             //if there is one, kill it, else false.
-            var canJump = this.canJump(tile);
+            var canJump = this.canJump(tile.position);
+            //Get piece in between
             if (canJump) {
-                for (pieceIndex in pieces) {
-                    if (pieces[pieceIndex].position[0] === tileToChecky && pieces[pieceIndex].position[1] === tileToCheckx) {
-                        if (this.owner !== pieces[pieceIndex].owner) {
-                            pieces[pieceIndex].remove();
-                            return true;
-                        }
-                    }
-                }
-                //TODO: somehow we didn't find the piece, is this possible?
-                return false;
+                console.log("can jump");
+                canJump.remove();
+                return true;
             } else {
+                console.log("can't jump");
                 return false;
             }
 
@@ -130,6 +154,11 @@ window.onload = function() {
             //TODO: reset piece position
             Board.redraw();
             //TODO: use websocket to send spring whatever new information via board object
+            this.linkedElement.css("display", "none");
+            Board.board[this.position[0]][this.position[1]] = 0;
+            //reset position so it doesn't get picked up by the for loop in the canOpponentJump method
+            this.position = [];
+            //TODO: determine winner
         };
     }
 
@@ -140,8 +169,7 @@ window.onload = function() {
         this.validTile = function (piece) {
             if (distance(this.position[0], this.position[1], piece.position[0], piece.position[1]) === Math.sqrt(2)) {
                 return ActionEnum.move;
-            } else if (dist(this.position[0], this.position[1], piece.position[0], piece.position[1]) === 2 * Math.sqrt(2)) {
-
+            } else if (distance(this.position[0], this.position[1], piece.position[0], piece.position[1]) === 2 * Math.sqrt(2)) {
                 return ActionEnum.jump;
             }
         };
@@ -150,7 +178,6 @@ window.onload = function() {
     var Board = {
         board: boardstate,
         playerTurn: PlayerTurnEnum.red,
-        //TODO: potentially useless????
         tilesElement: $('div.tiles'),
         //TODO: potential fucking magic?
         //dictionary to convert position in Board.board to the viewport units
@@ -176,12 +203,12 @@ window.onload = function() {
                         }
                     }
                     if(this.board[row][column] === 1) {
-                        $('.player1pieces').append("<div class='piece' id='"+pieceCount+"' style='top:"+this.dictionary[row]+";left:"+this.dictionary[column]+";'></div>");
-                        pieces[pieceCount] = new Piece($("#"+pieceCount), [parseInt(row), parseInt(column)]);
+                        $('.player1pieces').append("<div class='piece' id='" + pieceCount + "' style='top:"+this.dictionary[row]+";left:"+this.dictionary[column]+";'></div>");
+                        pieces[pieceCount] = new Piece($("#" + pieceCount), [parseInt(row), parseInt(column)], PlayerTurnEnum.red);
                         pieceCount += 1;
                     } else if(this.board[row][column] === 2) {
-                        $('.player2pieces').append("<div class='piece' id='"+pieceCount+"' style='top:"+this.dictionary[row]+";left:"+this.dictionary[column]+";'></div>");
-                        pieces[pieceCount] = new Piece($("#"+pieceCount), [parseInt(row), parseInt(column)]);
+                        $('.player2pieces').append("<div class='piece' id='" + pieceCount+ "' style='top:"+this.dictionary[row]+";left:"+this.dictionary[column]+";'></div>");
+                        pieces[pieceCount] = new Piece($("#" + pieceCount), [parseInt(row), parseInt(column)], PlayerTurnEnum.black);
                         pieceCount += 1;
                     }
                 }
@@ -207,6 +234,9 @@ window.onload = function() {
         clear: function () {
             //TODO: resets the game board???? toggleable via switch?
             location.reload();
+        },
+        redraw: function () {
+            console.log("Yeah does nothing");
         }
     };
 
@@ -220,7 +250,6 @@ window.onload = function() {
     //select the piece on click if it is the player's turn
     $('.piece').on("click", function () {
         var selected;
-        //TODO: not sure what this does
         var isPlayersTurn = ($(this).parent().attr("class").split(' ')[0] === "player" + Board.playerTurn + "pieces");
         if(isPlayersTurn) {
             if($(this).hasClass('selected')) {
@@ -255,6 +284,10 @@ window.onload = function() {
                 if(inRange === ActionEnum.jump) {
                     if(piece.jump(tile)) {
                         piece.move(tile);
+                        if (piece.canJumpAgain()) {
+                            Board.switchTurn();
+                            piece.linkedElement.addClass('selected');
+                        }
                         //TODO: this logic is garbage, change it to be that if a player jumps, instead check for more jumps.
                         //TODO: if there are, don't do anything and let them click, and give option to move, otherwise end turn
                         /*if(piece.canJumpAny()) {
@@ -264,8 +297,11 @@ window.onload = function() {
                     }
                     //if it's regular then move it if no jumping is available
                 } else if(inRange === ActionEnum.move) {
+                    console.log("move");
                     piece.move(tile);
                 }
+            } else {
+                console.log("fuck not in range");
             }
         }
     });
